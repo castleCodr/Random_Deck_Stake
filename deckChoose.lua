652bc9e59cc63e1286c78a80500130a1fd9a1858
castleCodr <118486854+castleCodr@users.noreply.github.com>
2024-07-05 07:12:26 -0500

    Add files via upload

--- deckChoose.lua
+++ deckChoose.lua
@@ -1,28 +1,63 @@
 Controller_key_update_ref = Controller.key_hold_update
-function Controller:key_hold_update(key, dt)
+function Controller:key_hold_update(key, dt)    
     if ((self.locked) and not G.SETTINGS.paused) or (self.locks.frame) or (self.frame_buttonpress) then return end
     Controller_key_update_ref(self,key,dt)
-    if(key == "d" and G.GAME.viewed_back ~= nil) then
+    if(G.GAME.viewed_back == nil or G.GAME.viewed_back.effect == nil or G.GAME.viewed_back.effect.center == nil  and get_deck_win_stake(G.GAME.viewed_back.effect.center.key) ~= nil and get_deck_win_stake(G.GAME.viewed_back.effect.center.key) ~= 0) then
+        return
+    end
+    G.FUNCS.random_back = function() 
+        
+        args = {to_key = math.random(1,#G.P_CENTER_POOLS['Stake'])}
+        G.viewed_stake = G.viewed_stake or 1
+        G.GAME.viewed_back:change_to(G.P_CENTER_POOLS.Back[args.to_key])
+        if G.sticker_card then G.sticker_card.sticker = get_deck_win_sticker(G.GAME.viewed_back.effect.center) end
+        local max_stake = get_deck_win_stake(G.GAME.viewed_back.effect.center.key) or 0
+        if(max_stake == 0 and G.PROFILES ~= nil and G.PROFILES[G.SETTINGS.profile].MEMORY  ~= nil) then
+            G.viewed_stake = math.min(G.viewed_stake, max_stake + 1)
+            G.PROFILES[G.SETTINGS.profile].MEMORY.deck = args.to_val    
+        end
+    end
+    if(key == "d" and G.GAME.viewed_back ~= nil )then
         math.randomseed(os.clock())
         math.random()
         math.random()
         math.random()
-        G.FUNCS.change_viewed_back({to_key=math.random(1,#G.P_CENTER_POOLS['Back'])})
+        local back = {to_key=math.random(1,#G.P_CENTER_POOLS['Back']) }
+        if(back ~= nil) then
+            if(back.to_key ~= nil) then
+                G.FUNCS.random_back()       
+            end
+        end
+        
     end
-    if(key == "s" and G.GAME.stake ~= nil) then
+    if(key == "s" and G.GAME.stake ~= nil)  and get_deck_win_stake(G.GAME.viewed_back.effect.center.key) ~= nil then
         math.randomseed(os.clock())
         math.random()
         math.random()
         math.random()
-        G.FUNCS.change_stake({to_key=math.random(1,#G.P_CENTER_POOLS['Stake'])})
+        local stake = {to_key=math.random(1,#G.P_CENTER_POOLS['Stake'])}
+        if(stake ~= nil) then
+            if(stake.to_key ~= nil) then
+                G.FUNCS.change_stake(stake)         
+            end
+        end
     end
-    if(key == "r" and G.GAME.stake ~= nil and G.GAME.viewed_back ~= nil) then
+    if(key == "r" and G.GAME.stake ~= nil and G.GAME.viewed_back ~= nil) and get_deck_win_stake(G.GAME.viewed_back.effect.center.key) ~= nil then
         math.randomseed(os.clock())
         math.random()
         math.random()
         math.random()
-        G.FUNCS.change_viewed_back({to_key=math.random(1,#G.P_CENTER_POOLS['Back'])})
-        G.FUNCS.change_stake({to_key=math.random(1,#G.P_CENTER_POOLS['Stake'])})
-    end
+        if(back ~= nil) then
+            if(back.to_key ~= nil) then
+                G.FUNCS.random_back()       
+            end
+        end
+        local stake = {to_key=math.random(1,#G.P_CENTER_POOLS['Stake'])}
+        if(stake ~= nil) then
+            if(stake.to_key ~= nil) then
+                G.FUNCS.change_stake(stake)         
+            end
+        end
     
+    end
 end
